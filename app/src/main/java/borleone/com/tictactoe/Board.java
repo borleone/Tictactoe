faa6180d25159859a052cf2606b1ed149eb413c9
borleone <pushkar.borle@gmail.com>
2018-06-06 00:07:25 -0400

    two-player tictactoe

---
+++ app/src/main/java/borleone/com/tictactoe/Board.java
@@ -0,0 +1,102 @@
+package borleone.com.tictactoe;
+
+import android.support.v4.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+class Board {
+
+    private int[][] grid;
+    private int turn = 1;
+    private Stack<Integer> playOrder;
+    private ArrayList<Pair> win_places = new ArrayList<>();
+
+    Board() {
+        grid = new int[3][3];
+        playOrder = new Stack<>();
+    }
+
+    public void newGame() {
+        // Initialize all cells in grid
+        for (int[] row : grid)
+            Arrays.fill(row, 0);
+
+        // Clear stack of playing order
+        playOrder.clear();
+        if (!win_places.isEmpty()) win_places.clear();
+    }
+
+    public void play(int row, int col) {
+        grid[row][col] = turn;
+        turn = (turn == 1) ? 2 : 1;
+        playOrder.push((row * 3) + col);
+    }
+
+    public int getValue(int row, int col) {
+        return grid[row][col];
+    }
+
+    public int getResult() {
+        int i;
+        if (!win_places.isEmpty()) win_places.clear();
+
+        for (i = 0; i < 3; ++i)
+            if (grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2] && grid[i][0] != 0) {
+                win_places.add(new Pair<>(i, 0));
+                win_places.add(new Pair<>(i, 1));
+                win_places.add(new Pair<>(i, 2));
+                return grid[i][0];
+            }
+
+        for (i = 0; i < 3; ++i)
+            if (grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i] && grid[0][i] != 0) {
+                win_places.add(new Pair<>(0, i));
+                win_places.add(new Pair<>(1, i));
+                win_places.add(new Pair<>(2, i));
+                return grid[0][i];
+            }
+
+        if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && grid[0][0] != 0) {
+            win_places.add(new Pair<>(0, 0));
+            win_places.add(new Pair<>(1, 1));
+            win_places.add(new Pair<>(2, 2));
+            return grid[0][0];
+        }
+
+        if (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0] && grid[0][2] != 0) {
+            win_places.add(new Pair<>(0, 2));
+            win_places.add(new Pair<>(1, 1));
+            win_places.add(new Pair<>(2, 0));
+            return grid[0][2];
+        }
+
+        //Draw
+        if (playOrder.size() == 9)
+            return 0;
+
+        return -1;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public ArrayList<Pair> getWin_places() {
+        return win_places;
+    }
+
+    public Pair undo() {
+        // Undo disable for first turn
+        if (playOrder.empty()) return null;
+
+        int lastPlayed = playOrder.pop();
+        int row, col;
+        row = lastPlayed / 3;
+        col = lastPlayed % 3;
+        grid[row][col] = 0;
+        turn = (turn == 1) ? 2 : 1;
+        return new Pair<>(row, col);
+    }
+}
---
+++ app/src/main/java/borleone/com/tictactoe/MainActivity.java
@@ -0,0 +1,197 @@
+package borleone.com.tictactoe;
+
+import android.support.v4.util.Pair;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Board tictactoe;
+    private Button[][] mGrid;
+    private Button mNewGame, mUndo;
+    private TextView mGameStatus;
+    private boolean isGameActive;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGrid = new Button[3][3];
+        mGrid[0][0] = (Button) findViewById(R.id.grid_0);
+        mGrid[0][1] = (Button) findViewById(R.id.grid_1);
+        mGrid[0][2] = (Button) findViewById(R.id.grid_2);
+        mGrid[1][0] = (Button) findViewById(R.id.grid_3);
+        mGrid[1][1] = (Button) findViewById(R.id.grid_4);
+        mGrid[1][2] = (Button) findViewById(R.id.grid_5);
+        mGrid[2][0] = (Button) findViewById(R.id.grid_6);
+        mGrid[2][1] = (Button) findViewById(R.id.grid_7);
+        mGrid[2][2] = (Button) findViewById(R.id.grid_8);
+
+        mNewGame = (Button) findViewById(R.id.button_new_game);
+        mUndo = (Button) findViewById(R.id.button_undo);
+        mGameStatus = (TextView) findViewById(R.id.textView_status);
+
+        mGrid[0][0].setOnClickListener(this);
+        mGrid[0][1].setOnClickListener(this);
+        mGrid[0][2].setOnClickListener(this);
+        mGrid[1][0].setOnClickListener(this);
+        mGrid[1][1].setOnClickListener(this);
+        mGrid[1][2].setOnClickListener(this);
+        mGrid[2][0].setOnClickListener(this);
+        mGrid[2][1].setOnClickListener(this);
+        mGrid[2][2].setOnClickListener(this);
+        mNewGame.setOnClickListener(this);
+        mUndo.setOnClickListener(this);
+
+        tictactoe = new Board();
+        isGameActive = true;
+        tictactoe.newGame();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int row, col;
+
+        if (isGameActive) {
+            switch (v.getId()) {
+                case R.id.grid_0:
+                    row = 0;
+                    col = 0;
+                    if (tictactoe.getValue(row, col) == 0) {
+                        tictactoe.play(row, col);
+                        mGrid[row][col].setText(tictactoe.getValue(row, col) == 1 ? "X" : "O");
+                    }
+                    break;
+                case R.id.grid_1:
+                    row = 0;
+                    col = 1;
+                    if (tictactoe.getValue(row, col) == 0) {
+                        tictactoe.play(row, col);
+                        mGrid[row][col].setText(tictactoe.getValue(row, col) == 1 ? "X" : "O");
+                    }
+                    break;
+                case R.id.grid_2:
+                    row = 0;
+                    col = 2;
+                    if (tictactoe.getValue(row, col) == 0) {
+                        tictactoe.play(row, col);
+                        mGrid[row][col].setText(tictactoe.getValue(row, col) == 1 ? "X" : "O");
+                    }
+                    break;
+                case R.id.grid_3:
+                    row = 1;
+                    col = 0;
+                    if (tictactoe.getValue(row, col) == 0) {
+                        tictactoe.play(row, col);
+                        mGrid[row][col].setText(tictactoe.getValue(row, col) == 1 ? "X" : "O");
+                    }
+                    break;
+                case R.id.grid_4:
+                    row = 1;
+                    col = 1;
+                    if (tictactoe.getValue(row, col) == 0) {
+                        tictactoe.play(row, col);
+                        mGrid[row][col].setText(tictactoe.getValue(row, col) == 1 ? "X" : "O");
+                    }
+                    break;
+                case R.id.grid_5:
+                    row = 1;
+                    col = 2;
+                    if (tictactoe.getValue(row, col) == 0) {
+                        tictactoe.play(row, col);
+                        mGrid[row][col].setText(tictactoe.getValue(row, col) == 1 ? "X" : "O");
+                    }
+                    break;
+                case R.id.grid_6:
+                    row = 2;
+                    col = 0;
+                    if (tictactoe.getValue(row, col) == 0) {
+                        tictactoe.play(row, col);
+                        mGrid[row][col].setText(tictactoe.getValue(row, col) == 1 ? "X" : "O");
+                    }
+                    break;
+                case R.id.grid_7:
+                    row = 2;
+                    col = 1;
+                    if (tictactoe.getValue(row, col) == 0) {
+                        tictactoe.play(row, col);
+                        mGrid[row][col].setText(tictactoe.getValue(row, col) == 1 ? "X" : "O");
+                    }
+                    break;
+                case R.id.grid_8:
+                    row = 2;
+                    col = 2;
+                    if (tictactoe.getValue(row, col) == 0) {
+                        tictactoe.play(row, col);
+                        mGrid[row][col].setText(tictactoe.getValue(row, col) == 1 ? "X" : "O");
+                    }
+                    break;
+            }
+
+            mGameStatus.setText(tictactoe.getTurn() == 1 ? getString(R.string.x_turn) : getString(R.string.o_turn));
+
+            int result = tictactoe.getResult();
+            if (result >= 0) {
+                isGameActive = false;
+                String gameResult = null;
+                ArrayList<Pair> list_places;
+                switch (result) {
+                    case 0:
+                        gameResult = getString(R.string.game_draw);
+                        break;
+                    case 1:
+                        gameResult = getString(R.string.x_win);
+                        list_places = tictactoe.getWin_places();
+                        for (Pair coordinate : list_places)
+                            mGrid[(int) coordinate.first][(int) coordinate.second].setTextColor(getColor(R.color.dark_grey_blue));
+                        break;
+                    case 2:
+                        gameResult = getString(R.string.o_win);
+                        list_places = tictactoe.getWin_places();
+                        for (Pair coordinate : list_places)
+                            mGrid[(int) coordinate.first][(int) coordinate.second].setTextColor(getColor(R.color.dark_grey_blue));
+                        break;
+                }
+                mGameStatus.setText(gameResult);
+            }
+        }
+
+        switch (v.getId()) {
+            case R.id.button_new_game:
+                tictactoe.newGame();
+                isGameActive = true;
+                mGameStatus.setText(R.string.x_turn);
+                for (int i = 0; i < 3; ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        mGrid[i][j].setText("");
+                        mGrid[i][j].setTextColor(getColor(R.color.white));
+                    }
+                }
+                break;
+            case R.id.button_undo:
+                Pair position = tictactoe.undo();
+                if (position != null) {
+                    row = (int) position.first;
+                    col = (int) position.second;
+                    mGrid[row][col].setText("");
+                    mGameStatus.setText(tictactoe.getTurn() == 1 ? getString(R.string.x_turn) : getString(R.string.o_turn));
+                    if (!isGameActive) {
+                        ArrayList<Pair> list_places = tictactoe.getWin_places();
+                        for (Pair coordinate : list_places)
+                            mGrid[(int) coordinate.first][(int) coordinate.second].setTextColor(getColor(R.color.white));
+                        isGameActive = true;
+                    }
+                } else Toast.makeText(this, "Game not started", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
